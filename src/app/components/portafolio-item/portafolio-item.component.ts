0e42b810ed24dd73a6b3896d67e67f5a15275bb6
samzerox <sam.zerox88@gmail.com>
2017-08-11 15:33:04 -0500

    Componente portafolioItem creado

---
+++ src/app/components/portafolio-item/portafolio-item.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-portafolio-item',
+  templateUrl: './portafolio-item.component.html',
+  styles: []
+})
+export class PortafolioItemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
