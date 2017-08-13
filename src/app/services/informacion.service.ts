fbf44b0cbd45358a75b480e55d7a8ac788b6afc6
samzerox <sam.zerox88@gmail.com>
2017-08-13 17:03:41 -0500

    Servicio de Firebase listo

--- src/app/services/informacion.service.ts
+++ src/app/services/informacion.service.ts
@@ -6,13 +6,29 @@ export class InformacionService {
 
   info:any= {};
   cargada:boolean = false;
+  cargada_sobre_nosotros:boolean = false;
+  equipo:any[]=[];
 
   constructor(public http:Http) {
-      this.http.get("assets/data/info.pagina.json")
+      this.carga_info();
+      this.carga_sobre_nosotros();
+  }
+
+  carga_info(){
+    this.http.get("assets/data/info.pagina.json")
+              .subscribe(data=>{
+                  // console.log(data.json());
+                  this.cargada = true;
+                  this.info = data.json();
+              });
+  }
+
+  public carga_sobre_nosotros(){
+    this.http.get("https://paginaweb-6d81a.firebaseio.com/equipo.json")
                 .subscribe(data=>{
                     console.log(data.json());
-                    this.cargada = true;
-                    this.info = data.json();
+                    this.cargada_sobre_nosotros = true;
+                    this.equipo = data.json();
                 });
   }
 
